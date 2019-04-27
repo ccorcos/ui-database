0e27a36586d6112c2577700e56e27937d428fcb1
Chet Corcos <ccorcos@gmail.com>
2019-04-27 13:36:28 -0700

    I don't think  magic reactivity is the way to go. Need a cleaner database model and then probably use hooks

--- src/ui2.tsx
+++ src/ui2.tsx
@@ -1,22 +1,6 @@
 import * as _ from "lodash"
 import * as React from "react"
 import { render } from "react-dom"
-import { any } from "glamor"
-
-/*
-Simplest Synchronous UI Database
-
-db.get(table, id) -> record<table>
-
-Would be cool if the same record could conform to multiple tables
-
-db.get([table1, table2], id) -> record<table1 & table2>
-
-Most applications don't need any indexes. Things can just be caches and sorted/filtered on the fly.
-
-TODO: transactions to batch together sets and emits.
-TODO: fine-grained listeners on a path? maybe not. keep the database as simple and general purpose as possible.
-*/
 
 export function randomId() {
 	return Math.round(Math.random() * 1e20).toString()
@@ -134,6 +118,94 @@ export class Database<TableToRecord extends { [key: string]: any }> {
 	} = {}
 }
 
+// export interface Gettable<V> {
+// 	get(): V
+// }
+
+// export interface Settable<V> extends Gettable<V> {
+// 	set(v: V): void
+// }
+
+// Basic event emitter to keep track of dependencies
+export class Dependency {
+	listeners: Set<() => void> = new Set()
+	add(listener: () => void) {
+		this.listeners.add(listener)
+	}
+	remove(listener: () => void) {
+		this.listeners.delete(listener)
+	}
+	emit() {
+		this.listeners.forEach(listener => listener())
+	}
+}
+
+// A stack of dependencies that represent every .get() during a computation
+export const computations: Array<Set<Dependency>> = []
+
+// Adds its dependency to the currrent computation on .get() and emits on .set()
+export class ReactiveValue<V> {
+	value: V
+	dependency = new Dependency()
+	constructor(value: V) {
+		this.value = value
+	}
+	get(): V {
+		const computation = computations[0]
+		computation && computation.add(this.dependency)
+		return this.value
+	}
+	set(value: V): void {
+		this.value = value
+		this.dependency.emit()
+	}
+	update(fn: (v: V) => V): void {
+		this.set(fn(this.get()))
+	}
+}
+
+// A value that is derrived from other values
+export class ReactiveFunction<V> {
+	value: V
+	dependency = new Dependency()
+	computation = new Set<Dependency>()
+	fn: () => V
+	stale = true
+	constructor(fn: () => V) {
+		this.fn = fn
+	}
+	run() {
+		computations.push(new Set())
+		this.value = this.fn()
+		const computation = computations.shift()
+		if (!computation) {
+			throw new Error("this should never happen.")
+		}
+		this.stop()
+		computation.forEach(dep => dep.add(this.onUpdate))
+		this.computation = computation
+	}
+	onUpdate = () => {
+		this.stale = true
+		this.dependency.emit()
+	}
+	flush() {
+		if (this.stale) {
+			this.stale = false
+			this.run()
+		}
+	}
+	get(): V {
+		this.flush()
+		const deps = computations[0]
+		deps && deps.add(this.dependency)
+		return this.value
+	}
+	stop() {
+		this.computation.forEach(dep => dep.remove(this.onUpdate))
+	}
+}
+
 /**
  * A _Store_ encapsultates state and the state and behaviors of a component.
  */
@@ -141,6 +213,8 @@ export class Store<
 	TableToRecord extends { [key: string]: any },
 	Table extends keyof TableToRecord
 > {
+	dependency = new Dependency()
+
 	constructor(
 		public table: Table,
 		public id: string,
@@ -157,10 +231,13 @@ export class Store<
 		db.stores[table][id] = this
 	}
 	public get() {
+		const computation = computations[0]
+		computation && computation.add(this.dependency)
 		return this.db.get(this.table, this.id)
 	}
 	public set(value: TableToRecord[Table] | undefined) {
 		this.db.set(this.table, this.id, value)
+		this.dependency.emit()
 	}
 	public listen(onChange: (value: TableToRecord[Table] | undefined) => void) {
 		return this.db.listen(this.table, this.id, onChange)
